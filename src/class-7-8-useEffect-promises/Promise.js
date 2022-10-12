f4ef04ef0a38746bbf8f2e4519dfec0d1df2e864
Omraj <omraj7413@gmail.com>
2022-10-12 22:11:56 +0530

    class 8 : completed

--- src/class-7-8-useEffect-promises/Promise.js
+++ src/class-7-8-useEffect-promises/Promise.js
@@ -2,28 +2,38 @@ import React from 'react'
 
 function Promise() {
 
-    const [userMessages, setUserMessages] = React.useState('')
+    const [userMessages, setUserMessages] = React.useState()
     const userMessage = [];
 
-    fetch('https://learning-at-acciojob-default-rtdb.asia-southeast1.firebasedatabase.app/message.json')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        for (const key in data){
-            userMessage.push(data[key])
+    React.useEffect(()=> {
+        fetch('https://learning-at-acciojob-default-rtdb.asia-southeast1.firebasedatabase.app/message.json')
+        .then(response => response.json())
+        .then(data => {
+            for(const d in data){
+                userMessage.push(data[d])
+            }
+            setUserMessages(userMessage)
         }
-        setUserMessages(userMessage)
-    })
+        )
+    }, [])
 
   return (
     <div>Promise
+
         {
-            userMessages.map(message => {
-                return (
-                    <div>
-                        {message.userName} : {message.userMessage}
-                    </div>
-                ) 
+            userMessages && userMessages.map(msg => {
+                return <div
+                    style={{
+                        color: 'white',
+                        backgroundColor: 'orchid',
+                        padding: 20,
+                        margin: 10,
+                        borderRadius: 10,
+                        textAlign: 'center'
+                    }}
+                >
+                    {msg.userName}: {msg.userMessage}
+                </div>
             })
         }
     </div>
